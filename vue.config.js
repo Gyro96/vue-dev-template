fe2c4c1da4dfb496d2e6b4c4a3711c3eb076f6a6
garyxjh <garyxjh@gmail.com>
2021-07-08 00:55:47 +0800

    修改 style-resources-loader 配置

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
 const path = require("path");
 module.exports = {
-  pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss", // (表示作用于 scss)自动引入
-      patterns: [path.resolve(__dirname, "src/assets/common.scss")], // 在当前每个scss页面中导入common.scss（这里面主要存放全局变量）
-    },
+  // 在每个单文件组件中导入 common.scss
+  chainWebpack: (config) => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach((type) =>
+      addStyleResource(config.module.rule("scss").oneOf(type))
+    );
   },
 };
+
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [path.resolve(__dirname, "./src/assets/common.scss")],
+    });
+}
